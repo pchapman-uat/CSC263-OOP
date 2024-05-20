d67622033f07d0cfabe56c0637006b8f18b89915
Preston Chapman <pchapman82070@uat.edu>
2024-05-20 12:42:44 -0700

    Import Animal Class
    From: https://github.com/pchapman-uat/CSC263-Inheritance

--- Main.java
+++ Main.java
@@ -3,3 +3,20 @@ public class Main{
         System.out.println("Hello World");
     }
 }
+class Animal {
+    // Class variables
+    public int age;
+    public String species;
+
+    // Constructor
+    public Animal(String species, int age){
+        this.age = age;
+        this.species = species;
+    }
+
+    // Print information about the animal 
+    public void printInfo(){
+        System.out.println("My Species is: "+this.species);
+        System.out.println("My Age is: "+this.age);
+    }
+}
