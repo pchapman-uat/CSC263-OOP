3d3b683fc32310f094350d948456869f90a1e6be
Preston Chapman <pchapman82070@uat.edu>
2024-05-22 12:49:57 -0700

    Remove Limb Method
    Added Arms, Tentacles, and methods to remove limbs

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 public class Main{
     public static void main(String[] args) {
         System.out.println("Hello World");
-        Cephalopod cephalopod = new Cephalopod("Cuttlefish", 40);
+        Cephalopod cephalopod = new Cephalopod("Cuttlefish", 40, 8, 0);
         cephalopod.printInfo();
+        cephalopod.removeArm();
+        cephalopod.removeArm(2);
     }
 }
 
@@ -50,17 +52,22 @@ abstract class Animal {
 class Cephalopod extends Animal{
     // Class variables
     private String subSpecies;
+    private int arms;
+    private int tentacles;
     // NOTE: Assignment - Polymorphism
     // Constructor
-    public Cephalopod(String species, String subSpecies, int age){
+    public Cephalopod(String species, String subSpecies, int age, int arms, int tentacles){
         // Call the constructor of the base class
         super(species, age);
         // Set the additonal class variables
         this.subSpecies = subSpecies;
+        this.arms = arms;
+        this.tentacles = tentacles;
     }
     // Secondary constructor for without sub species
-    public Cephalopod(String species, int age){
+    public Cephalopod(String species, int age, int arms, int tentacles){
         super(species, age);
+        this.arms = arms;
     }
     // Swim method
     public void swim(float distance){
@@ -76,6 +83,12 @@ class Cephalopod extends Animal{
         } else {
             super.printInfo();
         }
+        if(this.arms > 0){
+            System.out.println("I have "+this.arms+" arms!");
+        }
+        if(this.tentacles>0){
+            System.out.println("I have "+this.tentacles+" tentacles!");
+        }
     }
 
     // NOTE: Assignment - Encapsulation
@@ -85,6 +98,39 @@ class Cephalopod extends Animal{
     }
     public void setSubSpecies(String subSpecies) {
         this.subSpecies = subSpecies;
+    }
+    public void removeArm(){
+       this.removeLimb(1, 1);
+    }
+    public void removeArm(int arms){
+        this.removeLimb(arms, 1);
+    }
+    public void removeTentacle(){
+        this.removeLimb(1, 0);
+    }
+    public void removeTentacle(int limbs){
+        this.removeLimb(limbs, 0);
+    }
+    private void removeLimb(int change, int type){
+        int limbs;
+        String limb;
+        if(type == 0){
+            limbs = this.tentacles;
+            limb = "tentacle";
+        }else if(type == 1){
+            limbs = this.arms;
+            limb = "arm";
+        } else return;
+        if(limbs>0) {
+            limbs -= change;
+            if(this.arms < 0) this.arms = 0;
+            System.out.println("I lost "+change+" "+limb+"(s)!");
+            if(type == 0) this.tentacles = limbs;
+            else if (type == 1) this.arms = limbs;
+            System.out.println("I have "+this.arms+" "+limb+"(s) left!");
+        } else {
+            System.out.println("I have no "+limb+"s to lose!");
+        }
     }
 }
 
